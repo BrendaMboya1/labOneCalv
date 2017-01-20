aa6c88a7a96da6d76d43430186ab6230acc6e942
brenda.mboya <brendamboya.mboya@gmail.com>
2017-01-20 20:37:53 +0300

    I have added the subtract functions

--- labOneCalculator.java
+++ labOneCalculator.java
@@ -7,6 +7,13 @@ public static void main (String []a){
      double ans= one+ two;
      System.out.println("the sum of the two numbers is "+ans);
   }
+  if(a[0].equals("subtract")){
+    double one= Double.parseDouble(a[1]);
+     double two= Double.parseDouble(a[2]);
+     double ans= one-two;
+     System.out.println("the difference of the two numbers is : "+ans);
+  }
+  
   /*Scanner input=  new Scanner(System.in);
   
   System.out.println ("Welcome to my calculator.My calulator can:\n 1.add \n 2.divide");
