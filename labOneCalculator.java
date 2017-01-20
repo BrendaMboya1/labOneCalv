fe63c68a07f1edcffc05b12a5527d5d312494e7e
brenda.mboya <brendamboya.mboya@gmail.com>
2017-01-20 20:30:10 +0300

    Brenda has written the add function

---
+++ labOneCalculator.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class labOneCalculator{
+public static void main (String []a){
+  if(a[0].equals("add")){
+    double one= Double.parseDouble(a[1]);
+     double two= Double.parseDouble(a[2]);
+     double ans= one+ two;
+     System.out.println("the sum of the two numbers is "+ans);
+  }
+  /*Scanner input=  new Scanner(System.in);
+  
+  System.out.println ("Welcome to my calculator.My calulator can:\n 1.add \n 2.divide");
+  int a= input.nextInt();
+  int b= input.nextInt();
+  **/
+}
+}
