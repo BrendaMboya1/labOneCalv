9ae83cd3823088014e79e9a91e13ed118986f272
Constant Likudie <likudie@gmail.com>
2017-01-23 10:33:55 +0000

    Division done

--- labOneCalculator.java
+++ labOneCalculator.java
@@ -5,27 +5,38 @@
 
 import java.util.*;
 public class labOneCalculator{
-public static void main (String []a){
-  if(a[0].equals("add")){
-    double one= Double.parseDouble(a[1]);
-     double two= Double.parseDouble(a[2]);
-     double ans= one+ two;
-     System.out.println("the sum of the two numbers is "+ans);
-  }
+	public static void main (String []a){
+		if(a[0].equals("add")){
+			double one= Double.parseDouble(a[1]);
+			double two= Double.parseDouble(a[2]);
+			double ans= one+ two;
+			System.out.println("the sum of the two numbers is "+ans);
+		}
 
-  if(a[0].equals("subtract")){
-    double one= Double.parseDouble(a[1]);
-     double two= Double.parseDouble(a[2]);
-     double ans= one-two;
-     System.out.println("the difference of the two numbers is : "+ans);
-  }
-   if(a[0].equals("multiply")){
-    double one= Double.parseDouble(a[1]);
-     double two= Double.parseDouble(a[2]);
-     double ans= one*two;
-     System.out.println("the product of the two numbers is "+ans);
+		if(a[0].equals("subtract")){
+			double one= Double.parseDouble(a[1]);
+			double two= Double.parseDouble(a[2]);
+			double ans= one-two;
+			System.out.println("the difference of the two numbers is : "+ans);
+		}
+		if(a[0].equals("multiply")){
+			double one= Double.parseDouble(a[1]);
+			double two= Double.parseDouble(a[2]);
+			double ans= one*two;
+			System.out.println("the product of the two numbers is "+ans);
 
-  }
+		}
+
+  /*
+  Division was worked on by Constant Komla Likudie
+  */
+  if(a[0].equals("divide")){
+			double one= Double.parseDouble(a[1]);
+			double two= Double.parseDouble(a[2]);
+			double ans= one/two;
+			System.out.println(one + " divided by " + two + " is: " +ans);
+
+		}
   
   /*Scanner input=  new Scanner(System.in);
   
